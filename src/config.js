6a3b09ac4bc5338ecf5d2d2572b33089d055a8f7
Agustin Carrasco <asermax@gmail.com>
2017-05-15 01:27:27 -0300

    refactor: use ES6 promises for mongoose, use a config module for env variables

---
+++ src/config.js
@@ -0,0 +1,6 @@
+/* global process */
+
+export const MONGO_URL = process.env.MONGO_URL
+export const PORT = process.env.PORT
+export const STORAGE_PATH = process.env.STORAGE_PATH
+export const MEDIA_PREFIX = process.env.MEDIA_PREFIX
--- src/server.js
+++ src/server.js
@@ -1,14 +1,18 @@
+/* globals Promise */
+
 import mongoose from 'mongoose'
 import express from 'express'
 import bodyParser from 'body-parser'
+import { MONGO_URL, PORT } from './config'
 import routes from './routes'
 
 // mongoose
-mongoose.connect(process.env.MONGO_URL)
+mongoose.Promise = Promise
+mongoose.connect(MONGO_URL)
 
 // setup express instance
 const app = express()
-const port = process.env.PORT
+const port = PORT
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
